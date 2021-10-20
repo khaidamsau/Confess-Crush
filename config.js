78bdd164f484d8f2c9d89f375f9dc4c8d354f9ec
khaidamsau <92837641+khaidamsau@users.noreply.github.com>
2021-10-20 10:21:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ const CONFIG = {
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://facebook.com/khaikipthoi' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
